d1b2cb07d97f97c4985b101d1bdc673feaa00e7a
phphuc0608 <phuc31082002@gmail.com>
2024-06-17 16:58:32 +0700

    update route

--- src/routers/router.js
+++ src/routers/router.js
@@ -4,6 +4,11 @@ const routes = [
     path: '/',
     name: 'InputView',
     component: () => import('../views/InputView.vue')
+  },
+  {
+    path: '/graduation-card',
+    name: 'GraduationCard',
+    component: () => import('../views/GraduationCard.vue')
   }
 ];
 const router = createRouter({
